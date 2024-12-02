c625ba4bf2bad2760d3a94b1ddcba91de2013f2e
Vander Elme <vander@elme.eti.br>
2024-12-02 09:48:06 -0300

    20241202 atualizacao

--- NavegarAvcb.py
+++ NavegarAvcb.py
@@ -1,1 +1,8 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+navegador = webdriver.Firefox()
+
+navegador.get("https://suportesindico.com.br")
+navegador.maximize_window()
+navegador.find_element()
